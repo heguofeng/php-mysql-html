70d4450ea71de823a19c7927dd0efc37bcaed40f
heguofeng <32927202@qq.com>
2017-06-28 20:02:53 +0800

    可恶的黑客！！！

--- bysjWeb/configs/configs.php
+++ bysjWeb/configs/configs.php
@@ -1,8 +1,8 @@
 <?php 
 
 define("DB_HOST","123.206.100.251");
-define("DB_USER","hgf");
-define("DB_PWD","hgf");
+define("DB_USER","heguofeng");
+define("DB_PWD","heguofeng3930");
 define("DB_DBNAME","bysj");
 define("DB_CHARSET","utf8");
 
