5afb67bd1059a4673741c61ba8e7cbc553746309
heguofeng <32927202@qq.com>
2017-07-12 22:38:09 +0800

    修改密码

--- bysjWeb/configs/configs.php
+++ bysjWeb/configs/configs.php
@@ -2,7 +2,7 @@
 
 define("DB_HOST","123.206.100.251");
 define("DB_USER","heguofeng");
-define("DB_PWD","heguofeng3930");
+define("DB_PWD","hgf3930..");
 define("DB_DBNAME","bysj");
 define("DB_CHARSET","utf8");
 
