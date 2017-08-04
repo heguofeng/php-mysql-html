a7f26a9d62f6d75c5c8233cee41684769c2ff616
heguofeng <32927202@qq.com>
2017-08-04 21:02:20 +0800

    psw

--- bysjWeb/configs/configs.php
+++ bysjWeb/configs/configs.php
@@ -1,8 +1,8 @@
 <?php 
 
 define("DB_HOST","123.206.100.251");
-define("DB_USER","heguofeng");
-define("DB_PWD","heguofeng");
+define("DB_USER","hgf");
+define("DB_PWD","hgf");
 define("DB_DBNAME","bysj");
 define("DB_CHARSET","utf8");
 //本地
